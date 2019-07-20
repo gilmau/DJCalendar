0a95b74a6736b4ba8bc3fea3443f824652bf4a2a
gilmau <maurer.gil@gmail.com>
2019-07-20 21:21:38 +0300

    add SignUpFragment

--- app/src/main/java/com/gilortal/djcalendar/Fragments/SignUpFormFragment.java
+++ app/src/main/java/com/gilortal/djcalendar/Fragments/SignUpFormFragment.java
@@ -1,13 +1,59 @@
 package com.gilortal.djcalendar.Fragments;
 
-import android.support.v4.app.Fragment;
 
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.gilortal.djcalendar.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class SignUpFormFragment extends Fragment {
+
+    TextInputEditText nameText;
+    TextInputEditText emailText;
+    TextInputEditText passwordText;
+    TextInputEditText ConfirmPasswordText;
+    CheckBox isDJCheckBox;
+    EditText aboutBox;
+
+    RadioButton genereButton1, genereButton2, genereButton3;
+
+
+
+
 
     public SignUpFormFragment() {
         // Required empty public constructor
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_signupform, container, false);
+
+
+        nameText = v.findViewById(R.id.name_sign_up_form_ID);
+        emailText = v.findViewById(R.id.email_sign_up_form_ID);
+        passwordText = v.findViewById(R.id.password_sign_up_form_ID);
+        ConfirmPasswordText = v.findViewById(R.id.password_confirm_sign_up_form_ID);
+        isDJCheckBox = v.findViewById(R.id.dj_or_not_button_ID);
+        aboutBox = v.findViewById(R.id.about_text_id);
+
+
+
+
+        return v;
+    }
 }
-
-
-
