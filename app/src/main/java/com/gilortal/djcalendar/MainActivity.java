ddb791ec6ffdc02bf0f1324fc16dfa5afe8fcede
gilmau <maurer.gil@gmail.com>
2019-07-20 18:01:22 +0300

    add SignUpFragment

--- app/src/main/java/com/gilortal/djcalendar/MainActivity.java
+++ app/src/main/java/com/gilortal/djcalendar/MainActivity.java
@@ -1,7 +1,7 @@
 package com.gilortal.djcalendar;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
