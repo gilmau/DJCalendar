dda96f8c521ff5f4ee4c9b461933de92815929a2
gilmau <maurer.gil@gmail.com>
2019-07-20 16:36:36 +0300

    added intarface LoginAuth signin Logic on MainActivity

--- app/src/main/java/com/gilortal/djcalendar/Fragments/LoginFragment.java
+++ app/src/main/java/com/gilortal/djcalendar/Fragments/LoginFragment.java
@@ -3,6 +3,7 @@ package com.gilortal.djcalendar.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.gilortal.djcalendar.Interfaces.LoginAuth;
 import com.gilortal.djcalendar.Interfaces.MoveToFrag;
 import com.gilortal.djcalendar.Interfaces.UpdateToServer;
 import com.gilortal.djcalendar.R;
@@ -26,7 +28,7 @@ public class LoginFragment extends Fragment {
     TextInputEditText passwordText;
     public LoginAuth loginAuth;
 
-    private OnFragmentInteractionListener mListener;
+ //   private OnFragmentInteractionListener mListener;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -38,7 +40,7 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,44 +61,6 @@ public class LoginFragment extends Fragment {
 
         return view;
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-
-        void onFragmentInteraction(Uri uri);
-    }
 }
+
+
